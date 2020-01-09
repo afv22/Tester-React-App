151431554e2e90ddbb818f053f894caff3af5751
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-01-09 00:04:56 -0500

    Slice 8: Modularization

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-
-
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-const days = ['M', 'Tu', 'W', 'Th', 'F'];
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+import CourseList, { addScheduleTimes } from './components/CourseList';
 
 // Firebase Info
 const firebaseConfig = {
@@ -62,130 +58,6 @@ const Banner = ({ user, title }) => (
   </React.Fragment>
 );
 
-// Course Management
-const getCourseTerm = course => (
-  terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-  course.id.slice(1, 4)
-);
-
-const daysOverlap = (days1, days2) => ( 
-  days.some(day => days1.includes(day) && days2.includes(day))
-);
-
-const hoursOverlap = (hours1, hours2) => (
-  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
-);
-
-const timeConflict = (course1, course2) => (
-  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
-);
-
-const courseConflict = (course1, course2) => (
-  course1 !== course2
-  && getCourseTerm(course1) === getCourseTerm(course2)
-  && timeConflict(course1, course2)
-);
-
-const hasConflict = (course, selected) => {
-  console.log(course, selected);
-  return (
-  selected.some(selection => courseConflict(course, selection))
-)};
-
-const buttonColor = selected => (
-  selected ? 'button is-success is-selected' : 'button'
-);
-
-const TermSelector = ({ state }) => (
-  <Button.Group hasAddons>
-    { Object.values(terms)
-        .map(value => 
-            <Button key={value} 
-              color={buttonColor(value === state.term)}
-              onClick={ () => state.setTerm(value)}
-              >
-              { value }
-            </Button>
-        )
-    }
-  </Button.Group>
-);
-
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match ? {} : {
-    days,
-    hours: {
-      start: hh1 * 60 + mm1 * 1,
-      end: hh2 * 60 + mm2 * 1
-    }
-  };
-};
-
-const saveCourse = (course, meets) => {
-  db.child('courses').child(course.id).update({meets})
-    .catch(error => alert(error));
-};
-
-const moveCourse = course => {
-  const meets = prompt('Enter new meeting data, in this format:', course.meets);
-  if (!meets) return;
-  const {days} = timeParts(meets);
-  if (days) saveCourse(course, meets); 
-  else moveCourse(course);
-};
-
-const Course = ({ course, state, user }) => (
-  <Button color={ buttonColor(state.selected.includes(course)) }
-    onClick={ () => state.toggle(course) }
-    onDoubleClick={ user ? () => moveCourse(course) : null }
-    disabled={ hasConflict(course, state.selected) }
-    >
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </Button>
-);
-
-const useSelection = () => {
-  const [selected, setSelected] = useState([]);
-  const toggle = (x) => {
-    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
-  };
-  return [ selected, toggle ];
-};
-
-// Always include a key when building components in loops. The key should be unique and constant (normally an id)
-// React.Fragment allows for multiple components to be returned without generating an unnecessary HTML wrapper
-const CourseList = ({ courses, user }) => {
-  const [term, setTerm] = useState('Fall');
-  const [selected, toggle] = useSelection();
-  const termCourses = courses.filter(course => term === getCourseTerm(course));
-
-  return (
-    <React.Fragment>
-      <TermSelector state={ { term, setTerm } } />
-      <Button.Group>
-        { termCourses.map(course =>
-           <Course key={ course.id } course={ course }
-             state={ { selected, toggle } }
-             user={ user } />) }
-      </Button.Group>
-    </React.Fragment>
-  )
-};
-
-const addCourseTimes = course => ({
-  ...course,
-  ...timeParts(course.meets)
-});
-
-const addScheduleTimes = schedule => ({
-  title: schedule.title,
-  courses: Object.values(schedule.courses).map(addCourseTimes)
-});
-
 // Components (Banner, CourseList) must be capitalized. They call the corresponding function
 // useEffect gets called when the state of any of the elements in the arg2 list are updated
 //     When empty, it's only called on the initial render
@@ -214,3 +86,4 @@ const App = () => {
 };
 
 export default App;
+export { db };
